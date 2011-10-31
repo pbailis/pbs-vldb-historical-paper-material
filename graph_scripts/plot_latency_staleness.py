1099c0ee014274d48aa6e87b8f893856b02d8e81
Peter Bailis <pbailis@cs.berkeley.edu>
2011-10-31 13:05:53 -0700

    updating graph scripts

--- graph_scripts/plot_latency_staleness.py
+++ graph_scripts/plot_latency_staleness.py
@@ -1,4 +1,53 @@
 
 from plot_utils import *
+from math import ceil
+from pylab import *
 
-fetch_results("../results/micro/5N-2011-10-30-15_20_59/")
+results = fetch_results("../results/micro/5N-2011-10-30-15_20_59/")
+
+percentile = .98
+
+def ktocolor(k):
+    if k == 0:
+        return 'black'
+    else:
+        return 'blue'
+
+def plot_with_errorbars(config, results):
+    #results[config][0] is ReadResults
+    #results[config][1] is writelatencies
+    chosen = results[config][0][pctile_index]
+    chosenlatency = average([r.latency for r in results[config][0][:pctile_index]])+average([latency for latency in results[config][1]])
+    chosenlatencydev = sqrt(pow(std([r.latency for r in results[config][0][:pctile_index]]), 2)+
+                            pow(std([latency for latency in results[config][1]]), 2))
+
+    errorbar([chosen.tstale], [chosenlatency], fmt='o', yerr=[chosenlatencydev], color=ktocolor(chosen.kstale))
+    text(chosen.tstale+.02, chosenlatency, "%dN%dR%dW" % (config.N, config.R, config.W), fontsize=8)
+
+for config in results:
+    pctile_index = int(ceil(len(results[config][0])*percentile))
+
+    ### nth percentile by t-staleness
+    order_by_t_stale(results[config][0])
+    plot_with_errorbars(config, results)
+
+    ### nth percentile by version
+    order_by_k_stale(results[config][0])
+    #plot_with_errorbars(config, results)
+
+    ### nth percentile by latency
+    order_by_latency(results[config][0])
+    #plot_with_errorbars(config, results)
+
+
+errorbar(10000, 0, fmt='o', color="black", label="K=1")
+errorbar(10000, 0, fmt='o', color="blue", label="K=2")
+legend(loc="upper right", numpoints=1)
+
+xlim(xmin=-5, xmax=150)
+title("N=%d, p_staler = %f" % (results.keys()[0].N, 1.0-percentile))
+xlabel("t-staleness (ms)")
+ylabel("average write + read latency (ms)")
+savefig("latency-stale.pdf")
+
+show()
--- graph_scripts/plot_utils.py
+++ graph_scripts/plot_utils.py
@@ -3,6 +3,8 @@ from read_result import *
 from config_settings import *
 from os import listdir
 
+NS_PER_MS = 1000000.0
+
 def fetch_results(resultsdir):
     resultsdict = {}
 
@@ -15,14 +17,23 @@ def fetch_results(resultsdir):
             N=int(d[0])
             R=int(d[2])
             W=int(d[4])
-            print N, R, W
 
             resultsdict[ConfigSettings(N, R, W)] = parse_file(resultsdir+"/"+d+"/"+s+"/cassandra.log")
 
     return resultsdict
 
+def order_by_t_stale(readlist):
+    readlist.sort(key=lambda res: res.tstale)
+
+def order_by_k_stale(readlist):
+    readlist.sort(key=lambda res: res.kstale)
+
+def order_by_latency(readlist):
+    readlist.sort(key=lambda res: res.latency)
+
 def parse_file(f):
     ret = []
+    write_latencies = []
     version_commits = {}
 
     #collect write info
@@ -34,12 +45,21 @@ def parse_file(f):
             write_end = int(line.split()[4].strip(','))
             last_committed_version = int(line.split()[5].strip(','))
 
-            version_commits[last_committed_version] = write_end
             assert last_committed_version == write_start_version
+            assert write_end-write_start
+
+            version_commits[last_committed_version] = write_end
+            write_latencies.append((write_end-write_start)/NS_PER_MS)
 
     #then collect read info; reads can come back before write commits!
     for line in open(f):
-        if line.find("RS") != -1:
+        if line.find("WS") != -1:
+            write_start = int(line.split()[4].strip(','))
+            write_start_version = int(line.split()[5].strip(','))
+        elif line.find("WC") != -1:
+            write_end = int(line.split()[4].strip(','))
+            last_committed_version = int(line.split()[5].strip(','))
+        elif line.find("RS") != -1:
             read_start = int(line.split()[4])
         elif line.find("RC") != -1:
             read_end = int(line.split()[4])
@@ -48,15 +68,15 @@ def parse_file(f):
             res = ReadResult(
                     #value
                     read_version,
-                    #k-staleness
-                    last_committed_version - read_version,
                     #t-staleness
-                    read_end - version_commits[read_version],
+                    (read_end - version_commits[read_version])/NS_PER_MS,
+                    #k-staleness
+                    read_version - last_committed_version,
                     #latency
-                    read_end - read_start)
+                    (read_end - read_start)/NS_PER_MS)
 
             assert res.latency > 0
 
             ret.append(res)
 
-    return ret
+    return [ret, write_latencies]
