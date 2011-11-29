9593e56a0ac49441ce6fa8894f7bf12234c9256b
Peter Bailis <pbailis@cs.berkeley.edu>
2011-11-28 18:37:40 -0800

    sweeping t-stales

--- sim-graphs/graphtstalecdf.py
+++ sim-graphs/graphtstalecdf.py
@@ -11,24 +11,32 @@ iters = 10000
 
 results = {}
 
-for config in configs:
-    run_sim(N, R, W, k, iters, config.simparams, "SWEEP", "tstale.txt")
-    t = []
-    stale = []
+for R in [1, 2]:
+    for W in [1, 2]:
+        if R+W > N:
+            continue
+        for config in configs:
+            run_sim(N, R, W, k, iters, config.simparams, "SWEEP", "tstale.txt")
+            t = []
+            stale = []
 
-    for line in open("tstale.txt"):
-        line = line.split()
-        t.append(float(line[0]))
-        stale.append(float(line[1]))
+            for line in open("tstale.txt"):
+                line = line.split()
+                t.append(float(line[0]))
+                stale.append(float(line[1]))
         
-    results[config] = [t, stale]
+            results[config] = [t, stale]
 
-for config in configs:
-    plot(results[config][1], results[config][0], config.markerfmt, label=config.name, color=config.color)
+        for config in configs:
+            plot(results[config][1], results[config][0], config.markerfmt[1:], label=config.name, color=config.color)
 
-xlabel("t-visibility (ms)")
-ylabel("1-p_stale")
+        xlabel("t-visibility (ms)")
+        ylabel("1-p_stale")
+        title("N=%d R=%d W=%d" % (N, R, W))
+        ylim(ymin=.65)
+        semilogx()
 
-legend(loc="lower right")
-savefig("tstales.pdf")
+        legend(loc="lower right")
+        savefig("tstales-%dN%dR%dW.pdf" % (N, R, W))
+        cla()
 
